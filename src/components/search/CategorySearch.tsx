06e3c397f7ffa99a3d5824bc31224452dcdb35bf
Silvia Prada Aprilia <silviaprada0904@gmail.com>
2024-06-26 20:11:01 +0700

    feat: category search

---
+++ src/components/search/CategorySearch.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function CategorySearch() {
+    return (
+        <View>
+            <Text>Category</Text>
+        </View>
+    )
+}
+
+export default CategorySearch;
